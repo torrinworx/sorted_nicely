064968393c7e84f96f5e6cd6ac194eaa10583104
Torrin Leonard <82110564+torrinworx@users.noreply.github.com>
2022-08-25 11:27:53 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+def sorted_nicely(list_to_sort: list):
+    """ Sort the given iterable in the way that humans expect."""
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(list_to_sort, key=alphanum_key)
